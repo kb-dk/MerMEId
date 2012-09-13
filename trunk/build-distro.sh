b1d87076e19ba81156675b3f97d75d329d01e722
slu <slu@3c7ca1a1-f336-440e-b0c8-27b7de15a481>
2012-09-13 12:44:12 +0000

    building a war file
    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/xml-store-v2@203 3c7ca1a1-f336-440e-b0c8-27b7de15a481

--- trunk/build-distro.sh
+++ trunk/build-distro.sh
@@ -3,8 +3,13 @@
 rm mermeid.tar.bz2
 rm -rf MerMEId ; mkdir MerMEId
 
-tar cvf - `find . -type f -print | grep -v MerMEId | grep -v svn | grep -v cms `  | (cd MerMEId ; tar xvf - )
+tar cvf - `find . -type f -print | \
+    grep -v MerMEId | grep -v svn | \
+    grep -v cms `  | (cd MerMEId ; tar xvf - )
 
+(cd MerMEId/mermeid ; jar cvf ../editor.war .)
 tar jcvf mermeid.tar.bz2 MerMEId
+
+
 
 
