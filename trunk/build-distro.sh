6d4f9d3a7fdd37365820b64fc9a48b01821ae2cb
slu <slu@3c7ca1a1-f336-440e-b0c8-27b7de15a481>
2012-09-19 10:28:03 +0000

    OK, now we turn to other problems
    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/xml-store-v2@240 3c7ca1a1-f336-440e-b0c8-27b7de15a481

--- trunk/build-distro.sh
+++ trunk/build-distro.sh
@@ -8,7 +8,9 @@ rm mermeid.tar.bz2
 rm -rf MerMEId ; mkdir MerMEId
 
 tar cvf - `find . -type f -print | \
-    grep -v MerMEId | grep -v svn | \
+    grep -v MerMEId | \
+    grep -v svn | \
+    grep -v local_config | \
     grep -v cms `  | (cd MerMEId ; tar xvf - )
 
 (cd MerMEId/mermeid ; jar cvf ../editor.war .)
