28f6f3954035dad936e5cb5bb0d065e5b1e2ec10
slu <slu@3c7ca1a1-f336-440e-b0c8-27b7de15a481>
2013-03-05 14:17:43 +0000

    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/xml-store-v2@502 3c7ca1a1-f336-440e-b0c8-27b7de15a481

--- trunk/clean.sh
+++ trunk/clean.sh
@@ -4,4 +4,13 @@
 # Before building a distribution we need a very clean start
 #
 
+# edit this, in case you don't have maven in your path, but installed
+# somewhere nonstandard
+export PATH="$HOME/mvnsh/bin/mvn:$PATH"
+
+#############
+# No configurations below
+#
+
 rm -rf build_dir distro_tar MerMEId mermeid.tar.bz2
+(cd filter ; mvn clean)
