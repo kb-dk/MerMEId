4c93c2cabd1b78fe45b540b494349c99ea4f116c
slu <slu@3c7ca1a1-f336-440e-b0c8-27b7de15a481>
2014-04-03 11:36:48 +0000

    does the first part of it
    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/xml-store-v2@700 3c7ca1a1-f336-440e-b0c8-27b7de15a481

--- trunk/xqueries/mirror.pl
+++ trunk/xqueries/mirror.pl
@@ -9,7 +9,17 @@ my $suri   = "dcm-udv-01.kb.dk:8080/exist/rest";
 my $turi   = "dcm-udv-01.kb.dk:8080/exist/rest";
 my $source = "/db/public";
 my $target = "/db/cnw/data";
-my $files  = "http://dcm-udv-01.kb.dk/storage/document-names.xq?c=CNW&db=".$source;
+my $since  = "2014-01-16T15:19:55";
+my $before = "2099-12-31T24:00:00";
+my $files  = "http://dcm-udv-01.kb.dk/storage/document-names.xq?" .
+    join ("&",
+	  (
+	   "c=CNW",
+	   "db=$source",
+	   "since=$since",
+	   "before=$before"
+	  )
+    );
 
 my $ua = LWP::UserAgent->new;
 $ua->agent("crud-client/0.1 ");
@@ -28,24 +38,25 @@ my $res = $ua->request($req);
 ## Check the outcome of the response
 
 if ($res->is_success) {
-#    print STDERR "Success getting file list " . $res->status_line . "\n";
+    print STDERR "Success getting $files list " . $res->status_line . "\n";
     my @filelist = split("\n",$res->content());
 
-    foreach my $file (@filelist) {
+    foreach my $line (@filelist) {
+	my ($file,$date) = split /\s+/,$line;
 	my $req = new HTTP::Request();
 	my $url = $scheme . $suri . $source .'/' .  $file;
 	$req->method("HEAD");
 	$req->uri($url);
 	my $res = $ua->request($req);
 	if ($res->is_success) {
-	    print STDERR "$file :" . $res->status_line . "\n";
+	    print STDERR "$url :" . $res->status_line . "\n";
 	    # print STDERR $res->headers()->as_string();
 	    # print STDERR $res->header( "Last-Modified" );
 	} else {
 	    print STDERR "$file :" . $res->status_line . " bah :( \n";
 	}
-
     }
-
+} else {
+	die "mirror.pl has problems with $files URI";
 }
 
