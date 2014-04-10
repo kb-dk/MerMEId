f036319ea2b05b2eb64a92001695de542f43446e
slu <slu@3c7ca1a1-f336-440e-b0c8-27b7de15a481>
2014-04-10 08:18:45 +0000

    no comments
    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/xml-store-v2@703 3c7ca1a1-f336-440e-b0c8-27b7de15a481

--- trunk/xqueries/mirror.pl
+++ trunk/xqueries/mirror.pl
@@ -5,6 +5,7 @@ use LWP::UserAgent;
 use Getopt::Long;
 use DateTime;
 
+
 my $scheme    = "http://";
 my $source_host_port = "dcm-udv-01.kb.dk:8080";
 my $target_host_port = "dcm-udv-01.kb.dk:8080";
@@ -14,9 +15,15 @@ my $suri      = $scheme . $source_host_port . "/exist/rest/db";
 my $turi      = $scheme . $target_host_port . "/exist/rest/db";
 my $source    = "/public";
 my $target    = "/cnw/data";
-my $edition   = "hartw";
+my $edition   = "cnw";
 my $since     = "2000-01-01T00:00:01";
 my $before    = "2099-12-31T24:00:00";
+
+#####
+#
+# Collecting data on source files
+#
+
 my $source_files     = $suri . "/document-info.xq?" .
     join ("&",
 	  (
@@ -27,51 +34,79 @@ my $source_files     = $suri . "/document-info.xq?" .
 	  )
     );
 
-my $ua = LWP::UserAgent->new;
-$ua->agent("crud-client/0.1 ");
-$ua->credentials($target_host_port, "exist" , $user, $password );
+my $target_files     = $turi . "/document-info.xq?" .
+    join ("&",
+	  (
+	   "c=$edition",
+	   "db=$target"
+	  )
+    );
 
-my $req = new HTTP::Request();
-$req->uri($source_files);
-$req->method("GET");
+my %sources = &get_data($source_files); 
+my %targets = &get_data($target_files); 
+my @filelist = keys(%sources);
 
-# $req->header( "Content-Type" => $suffixes{$suffix} );
+my $latest_modification = &parse_date_time("2000-01-01T00:00:01");
 
-# Pass request to the user agent and get a response back
+print "about to process $#filelist files\n";
 
-my $res = $ua->request($req);
+foreach my $file (@filelist) {
 
-## Check the outcome of the response
+    print STDERR "doing $file \n";
+    
+    my $docdate = &parse_date_time($sources{$file});
 
-if ($res->is_success) {
-    print STDERR "Success getting $source_files list " . $res->status_line . "\n";
-    my @filelist = split("\n",$res->content());
-
-    my $latest_modification = &parse_date_time("2000-01-01T00:00:01");
-    foreach my $line (@filelist) {
-	my ($file,$date) = split /\s+/,$line;
-	my $docdate = &parse_date_time($date);
-	if(DateTime->compare( $latest_modification, $docdate ) < 0 ) {
-	    $latest_modification = $docdate ;
-	}
-	my $req = new HTTP::Request();
-	my $target_url = $turi . $target .'/' .  $file;
-	my $source_url = $suri . $source .'/' .  $file;
-	$req->method("HEAD");
-	$req->uri($target_url);
-	my $res = $ua->request($req);
-	if ( $res->is_success() ) 
-	    &copy_file($source_url,$target_url);
-	} else {
-	    if($res->code() == 404) {
-		&copy_file($source_url,$target_url);
-	    }
-	}
+    if(DateTime->compare( $latest_modification, $docdate ) < 0 ) {
+	$latest_modification = $docdate ;
     }
 
-    print STDERR "$latest_modification\n";
-} else {
-	die "mirror.pl has problems with $source_files URI";
+    my $req = new HTTP::Request();
+    my $target_url = $turi . $target .'/' .  $file;
+    my $source_url = $suri . $source .'/' .  $file;
+
+    &copy_file($source_url,$target_url);
+
+}
+
+@filelist = keys(%targets);
+foreach my $file (@filelist) {
+    if($sources{$file}) {
+#	print STDERR "The file $file is in the source database\n";
+    } else {
+	print STDERR "The file $file is in the target database only should be removed\n";
+    }
+    
+}
+    
+sub get_data {
+    my $files = shift ;
+
+    my %data = ();
+
+    my $ua = LWP::UserAgent->new;
+    $ua->agent("crud-client/0.1 ");
+    $ua->credentials($target_host_port, "exist" , $user, $password );
+
+    my $req = new HTTP::Request();
+    $req->uri($files);
+    $req->method("GET");
+    my $res = $ua->request($req);
+
+    if ($res->is_success) {
+	print STDERR "Success getting $files list " . $res->status_line . "\n";
+	my @fileitems = split("\n",$res->content());
+
+
+	print STDERR "About to parse $#fileitems files\n";
+	
+	foreach my $line (@fileitems) {
+	    my ($file,$date) = split /\s+/,$line;
+	    $data{$file} = $date;
+	}
+
+    }
+
+    return %data;
 }
 
 
@@ -95,6 +130,7 @@ sub copy_file {
     my $source_url = shift;
     my $target_url = shift;
 
+    print STDERR "copy_file called\n";
     my $ua = LWP::UserAgent->new;
     my $getreq = new HTTP::Request();
     $getreq->method("GET");
