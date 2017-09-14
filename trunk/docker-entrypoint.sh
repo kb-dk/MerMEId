f3b375cc79c7410f3622bccd3e884c78f60cac59
Sigfrid Lundberg <siggelundberg@gmail.com>
2017-09-14 10:29:22 +0200

    these compiles and runs

--- trunk/docker-entrypoint.sh
+++ trunk/docker-entrypoint.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-pushd $WORKDIR
-./load_exist.pl --user admin --password 'ourpassword' --host-port localhost:8080 --load . --context /exist/rest/db/dcm --suffix xml,xq,xqm,css,xsl
-popd
+ls -l $CATALINA_HOME/bin/
+$CATALINA_HOME/bin/catalina.sh start
+export ME=`hostname`
+echo hostname = ${ME}
+echo `host ${ME}`
+ping localhost -c 3
+ping ${ME} -c 3
+cat /usr/local/tomcat/conf/server.xml
+
+lwp-request -m GET http://${ME}:8080/
+cd $WORKDIR
+./load_exist.pl --user admin  --host-port localhost:8080 --load . --context /exist/rest/db/dcm --suffix xml,xq,xqm,css,xsl
